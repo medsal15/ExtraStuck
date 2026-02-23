ea6ff8bd78e2c994ff94167a083d9cae3c102db3
medsal15 <medsal16+github@gmail.com>
2026-02-23 12:52:35 +0100

    improve doublingbowitem shooting method

--- src/main/java/com/medsal15/items/bows/DoublingBowItem.java
+++ src/main/java/com/medsal15/items/bows/DoublingBowItem.java
@@ -9,14 +9,23 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.projectile.AbstractArrow;
+import net.minecraft.world.entity.projectile.Projectile;
 import net.minecraft.world.item.BowItem;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
 
 public class DoublingBowItem extends BowItem {
     private boolean fakeArrow;
+    private final int copies;
 
     public DoublingBowItem(Properties properties) {
         super(properties);
+        copies = 1;
+    }
+
+    public DoublingBowItem(Properties properties, int copies) {
+        super(properties);
+        this.copies = copies;
     }
 
     @Override
@@ -24,9 +33,31 @@ public class DoublingBowItem extends BowItem {
             @Nonnull ItemStack weapon, @Nonnull List<ItemStack> projectileItems, float velocity, float inaccuracy,
             boolean isCrit, @Nullable LivingEntity target) {
         fakeArrow = false;
-        super.shoot(level, shooter, hand, weapon, projectileItems, velocity, inaccuracy, isCrit, target);
-        fakeArrow = true;
-        super.shoot(level, shooter, hand, weapon, projectileItems, velocity, inaccuracy * 2f, isCrit, target);
+        if (projectileItems.size() > 1 || copies <= 0) {
+            super.shoot(level, shooter, hand, weapon, projectileItems, velocity, inaccuracy, isCrit, target);
+            return;
+        }
+        float spread = EnchantmentHelper.processProjectileSpread(level, weapon, shooter, 0.0F);
+        float f1 = copies == 1 ? 0.0F
+                : 2.0F * spread / (float) (copies - 1);
+        float f2 = (float) ((copies - 1) % 2) * f1 / 2.0F;
+        float f3 = 1.0F;
+
+        for (int i = 0; i < copies + 1; i++) {
+            ItemStack stack = projectileItems.get(0);
+            if (!stack.isEmpty()) {
+                float f4 = f2 + f3 * (float) ((i + 1) / 2) * f1;
+                f3 = -f3;
+                Projectile projectile = this.createProjectile(level, shooter, weapon, stack, isCrit);
+                this.shootProjectile(shooter, projectile, i, velocity, inaccuracy, f4, target);
+                level.addFreshEntity(projectile);
+                weapon.hurtAndBreak(this.getDurabilityUse(stack), shooter, LivingEntity.getSlotForHand(hand));
+                if (weapon.isEmpty()) {
+                    break;
+                }
+                fakeArrow = true;
+            }
+        }
     }
 
     @Override
