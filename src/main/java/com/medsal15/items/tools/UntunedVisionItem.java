26f89fdd957b1061194105cf4e058a48698cb676
medsal15 <medsal16+github@gmail.com>
2026-07-28 20:43:08 +0200

    actually fix vision tuning

--- src/main/java/com/medsal15/items/tools/UntunedVisionItem.java
+++ src/main/java/com/medsal15/items/tools/UntunedVisionItem.java
@@ -29,44 +29,48 @@ public class UntunedVisionItem extends Item {
     public InteractionResultHolder<ItemStack> use(@Nonnull Level level, @Nonnull Player player,
             @Nonnull InteractionHand hand) {
         ItemStack stack = player.getItemInHand(hand);
-        if (ConfigServer.VISION_TUNING.getAsBoolean() && player instanceof ServerPlayer serverPlayer) {
-            int min = ConfigServer.VISION_RUNG.get();
-            Optional<Title> otitle = Title.getTitle(serverPlayer);
-            ItemStack result = ItemStack.EMPTY;
-            Component message = null;
-            if (min >= 0 && Echeladder.get(serverPlayer).getRung() < min) {
-                player.sendSystemMessage(Component.translatable(ESLangProvider.VISION_TUNE_POWERLESS));
-            } else if (otitle.isPresent()) {
-                Title title = otitle.get();
-                Holder<Item> item = switch (title.heroAspect()) {
-                    case SPACE -> ESItems.VISION_SPACE;
-                    case TIME -> ESItems.VISION_TIME;
-                    case MIND -> ESItems.VISION_MIND;
-                    case HEART -> ESItems.VISION_HEART;
-                    case HOPE -> ESItems.VISION_HOPE;
-                    case RAGE -> ESItems.VISION_RAGE;
-                    case BREATH -> ESItems.VISION_BREATH;
-                    case BLOOD -> ESItems.VISION_BLOOD;
-                    case LIFE -> ESItems.VISION_LIFE;
-                    case DOOM -> ESItems.VISION_DOOM;
-                    case LIGHT -> ESItems.VISION_LIGHT;
-                    case VOID -> ESItems.VISION_VOID;
-                };
-                result = new ItemStack(item, 1, stack.getComponentsPatch());
-                message = Component.translatable(ESLangProvider.VISION_TUNE);
-            } else if (!ConfigServer.VISION_TUNING_SAFE.getAsBoolean()) {
-                result = new ItemStack(ESItems.VISION_DULL, 1, stack.getComponentsPatch());
-                message = Component.translatable(ESLangProvider.VISION_TUNE_FAIL);
-            }
-
-            if (!result.isEmpty()) {
-                stack.consume(1, serverPlayer);
-                if (message != null)
-                    player.sendSystemMessage(message);
-                if (!player.getInventory().add(result)) {
-                    player.drop(result, false);
+        if (ConfigServer.VISION_TUNING.getAsBoolean()) {
+            if (player instanceof ServerPlayer serverPlayer) {
+                int min = ConfigServer.VISION_RUNG.get();
+                Optional<Title> otitle = Title.getTitle(serverPlayer);
+                ItemStack result = ItemStack.EMPTY;
+                Component message = null;
+                if (min >= 0 && Echeladder.get(serverPlayer).getRung() < min) {
+                    player.sendSystemMessage(Component.translatable(ESLangProvider.VISION_TUNE_POWERLESS));
+                } else if (otitle.isPresent()) {
+                    Title title = otitle.get();
+                    Holder<Item> item = switch (title.heroAspect()) {
+                        case SPACE -> ESItems.VISION_SPACE;
+                        case TIME -> ESItems.VISION_TIME;
+                        case MIND -> ESItems.VISION_MIND;
+                        case HEART -> ESItems.VISION_HEART;
+                        case HOPE -> ESItems.VISION_HOPE;
+                        case RAGE -> ESItems.VISION_RAGE;
+                        case BREATH -> ESItems.VISION_BREATH;
+                        case BLOOD -> ESItems.VISION_BLOOD;
+                        case LIFE -> ESItems.VISION_LIFE;
+                        case DOOM -> ESItems.VISION_DOOM;
+                        case LIGHT -> ESItems.VISION_LIGHT;
+                        case VOID -> ESItems.VISION_VOID;
+                    };
+                    result = new ItemStack(item, 1, stack.getComponentsPatch());
+                    message = Component.translatable(ESLangProvider.VISION_TUNE);
+                } else if (!ConfigServer.VISION_TUNING_SAFE.getAsBoolean()) {
+                    result = new ItemStack(ESItems.VISION_DULL, 1, stack.getComponentsPatch());
+                    message = Component.translatable(ESLangProvider.VISION_TUNE_FAIL);
                 }
-                return InteractionResultHolder.consume(stack);
+
+                if (!result.isEmpty()) {
+                    if (message != null)
+                        player.sendSystemMessage(message);
+                    if (!player.getInventory().add(result)) {
+                        player.drop(result, false);
+                    }
+                    stack.consume(1, serverPlayer);
+                    return InteractionResultHolder.success(stack);
+                }
+            } else {
+                return InteractionResultHolder.sidedSuccess(stack, true);
             }
         }
         return super.use(level, player, hand);
